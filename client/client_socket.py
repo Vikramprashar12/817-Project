79e85a7c31ebfbc3fdf8b5105519ea505a73a892
awaisirshad1 <awaisirshad1@icloud.com>
2025-04-05 15:04:28 -0400

    added requirements.txt

--- client/client_socket.py
+++ client/client_socket.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 def connect_to_server(host='localhost', port=8888):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,14 +7,28 @@ def connect_to_server(host='localhost', port=8888):
     return s
 
 def send_login(socket_conn, username, password):
-    socket_conn.sendall(f"LOGIN:{username}:{password}".encode())
+    message = {
+        "type": "login",
+        "username":f"{username}",
+        "password":f"{password}"
+    }
+    print(f"[DEBUG] Sending: {message}")
+    socket_conn.sendall(json.dumps(message).encode())
+    # socket_conn.sendall(f"LOGIN:{username}:{password}".encode())
     response = socket_conn.recv(1024).decode()
+    print(f"[DEBUG] Received from server: {response}")
     return response
 
 def send_register(socket_conn, username, password):
-    message = f"REGISTER:{username}:{password}"
+    # message = f"REGISTER:{username}:{password}"
+    message = {
+        "type": "signup",
+        "username":f"{username}",
+        "password":f"{password}"
+    }
     print(f"[DEBUG] Sending: {message}")
-    socket_conn.sendall(message.encode())
+
+    socket_conn.sendall(json.dumps(message).encode())
     response = socket_conn.recv(1024).decode()
     print(f"[DEBUG] Received from server: {response}")
     return response
